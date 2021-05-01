4f0ee510839e24ebdf004efb46660cf0972eef44
kevin <karvin_jimenez@hotmail.com>
2021-05-01 17:18:20 -0400

    Cambiando base de datos de mysql a h2

--- src/main/java/arkham/knight/gamestop/GamestopApplication.java
+++ src/main/java/arkham/knight/gamestop/GamestopApplication.java
@@ -7,7 +7,6 @@ import arkham.knight.gamestop.controllers.VideoGameController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 import java.io.File;
 
 @SpringBootApplication
